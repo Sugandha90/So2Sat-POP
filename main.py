37d3a176a5ce804cf397a9485ce47dc7bb3c8bbb
Doda <Sugandha.Doda@dlr.de>
2021-10-27 13:20:45 +0200

    'updated_main'

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from rf_classification import rf_classifier
 from rf_regression import rf_regressor
 from data_preprocessing import feature_engineering
-from constants import all_patches_mixed
+from constants import all_patches_mixed_part1, all_patches_mixed_part2
 
 if __name__ == '__main__':
 
-    # create features for training and testing data
-    feature_engineering(all_patches_mixed)
+    # create features for training and testing data from So2Sat POP Part1 and So2Sat POP Part2
+    feature_engineering(all_patches_mixed_part1)
+    feature_engineering(all_patches_mixed_part2)
 
     # Perform classification, ground truth is population class
     rf_classifier(file_name='rf_cls', ground_truth_col='CLASS')
